30c6edf1bf726a63880ac1d9a4642de3cb81dd9e
Kriss <krisacz@yahoo.com>
2016-06-24 17:24:51 +0100

    Works (?)

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Test
 {
@@ -16,11 +13,17 @@ namespace Test
             //var inputStr = "A"; //Result: A
             //var inputStr = "ABC"; //Result: ABC
             //var inputStr = "AB>CC"; //Result: ACB
-            //var inputStr = "AB>CC>FD>AE>BF"; //Result: AFCBDE
+            var inputStr = "AB>CC>FD>AE>BF"; //Result: AFCBDE
             //var inputStr = "ABC>C"; //Result: error - job can not depend on itself
             //var inputStr = "AB>CC>FD>AEF>B"; //Result: eror - circular dependenties
-            
-            for (int i = 0; i <= 10; i++)
+
+            //var inputStr = "A>BB>CC";
+            Console.WriteLine($"Input string: {inputStr}");
+            var resultString = Process(inputStr, true);
+            Console.WriteLine($"Result string: {resultString}");
+
+            /*
+            for (var i = 0; i <= 10; i++)
             {
                 var inputString = GenerateRandomInputString(i);
                 Console.WriteLine($"Input string [Count: {i}]: \t {inputString}");
@@ -29,13 +32,10 @@ namespace Test
                 Console.WriteLine();
                 Console.WriteLine("------------------------------------------------------------------------------------------");
             }
-
-            /*
-            var inputStr = "AB>CC>FD>AE>BF";
-            Console.WriteLine($"Input string: {inputStr}");            
-            var resultString = Process(inputStr, true);
-            Console.WriteLine($"Result string: {resultString}");
             */
+
+
+
 
             Console.ReadKey();
         }
@@ -53,7 +53,7 @@ namespace Test
             if(multiDependencyCharacter.IsMatch(inputStr)) return "ERROR: Incorrect input string. Dependency character used multiple times in a row.";
 
             //Last character is ">"
-            if (inputStr.Length > 0 && inputStr[inputStr.Length - 1] == '>') return "ERROR: Incorrect input string. Missing dependency task. Input string ends with \">\".";
+            if (inputStr.Length > 0 && inputStr[inputStr.Length - 1] == '>') return "ERROR: Incorrect input string. Missing dependency job. Input string ends with \">\".";
             #endregion
 
 
@@ -114,17 +114,22 @@ namespace Test
                 //Further validations are relying on dependency job being present so there is no point to carry on if there is none
                 if (string.IsNullOrWhiteSpace(dependencyJob1)) continue;
 
+                #region JOB DEPENDS ON IT SELF
+
+                if (string.Equals(job1, dependencyJob1)) return $"ERROR: Job can not depends on itself. [{job1} > {dependencyJob1}]";
+                #endregion
+
                 #region DEPENDENCY JOB NOT DEFINED                
-                var foundTask = false;
+                var foundJob = false;
                 foreach (var t2 in tokens)
                 {
                     if (string.Equals(dependencyJob1, t2.Item1))
                     {
-                        foundTask = true;
+                        foundJob = true;
                         break;
                     }
                 }
-                if (foundTask == false) return $"ERROR: Task {job1} depends on undefined {dependencyJob1} task.";
+                if (foundJob == false) return $"ERROR: Job {job1} depends on undefined {dependencyJob1} job.";
                 #endregion
 
                 #region CIRCULAR DEPENDENCIES                                            
@@ -147,45 +152,54 @@ namespace Test
             #region SORT JOBS
             //Mirror tokens list - this is the one we will be modifying as we can not modify an array while we are looping through it
             var sortedJobs = new List<Tuple<string, string>>(tokens);
-
-            //We could fully sort it by either "running" through all tokens twice or one decremental "run"
-            for(var a = tokens.Count - 1; a >= 0; a--)
+            
+            //We are processinf all tokens untill all tokens are in correct place
+            var correct = false;
+            while (!correct)
             {
-                var t = tokens[a];
-                var job = t.Item1;
-                var dependencyJob = t.Item2;
-                var jobIndex = sortedJobs.IndexOf(t);
-
-                //If job does't have dependency job check if any other jobs depends on it and place it before it (lowest possible index)
-                if (string.IsNullOrWhiteSpace(dependencyJob))
+                var changeMade = false;
+                for(var a = tokens.Count - 1; a >= 0; a--)
                 {
-                    //Initialize with maximum index
-                    var minimumIndex = tokens.Count - 1;
-                    for (var i = 0; i < sortedJobs.Count; i++)
-                    {
-                        var dependencyJob1 = sortedJobs[i].Item2;
-                        if (string.Equals(job, dependencyJob1)) minimumIndex = Math.Min(minimumIndex, i);
-                    }
+                    var t = tokens[a];
+                    var job = t.Item1;
+                    var dependencyJob = t.Item2;
+                    var jobIndex = sortedJobs.IndexOf(t);
 
-                    var newJobIndex = Math.Min(jobIndex, minimumIndex);
-                    sortedJobs.RemoveAt(jobIndex);
-                    sortedJobs.Insert(newJobIndex, t);
-                }
-                //...otherwise place current job after job it depends on
-                else
-                {
-                    //Initialize with maximum index
-                    var maximumIndex = 0;
-                    for (var i = 0; i < sortedJobs.Count; i++)
+                    //If job does't have dependency job check if any other jobs depends on it and place it before it (lowest possible index)
+                    if (string.IsNullOrWhiteSpace(dependencyJob))
                     {
-                        var job2 = sortedJobs[i].Item1;
-                        if (string.Equals(dependencyJob, job2)) maximumIndex = Math.Max(maximumIndex, i);
-                    }
+                        //Initialize with maximum index
+                        var minimumIndex = tokens.Count - 1;
+                        for (var i = 0; i < sortedJobs.Count; i++)
+                        {
+                            var dependencyJob1 = sortedJobs[i].Item2;
+                            if (string.Equals(job, dependencyJob1)) minimumIndex = Math.Min(minimumIndex, i);
+                        }
 
-                    var newJobIndex = Math.Max(jobIndex, maximumIndex);
-                    sortedJobs.RemoveAt(jobIndex);
-                    sortedJobs.Insert(newJobIndex, t);
+                        if (minimumIndex >= jobIndex) continue;
+                        sortedJobs.RemoveAt(jobIndex);
+                        sortedJobs.Insert(minimumIndex, t);
+                        changeMade = true;
+                    }
+                    //...otherwise place current job after job it depends on
+                    else
+                    {
+                        //Initialize with maximum index
+                        var maximumIndex = 0;
+                        for (var i = 0; i < sortedJobs.Count; i++)
+                        {
+                            var job2 = sortedJobs[i].Item1;
+                            if (string.Equals(dependencyJob, job2)) maximumIndex = Math.Max(maximumIndex, i);
+                        }
+
+                        if (maximumIndex <= jobIndex) continue;
+                        sortedJobs.RemoveAt(jobIndex);
+                        sortedJobs.Insert(maximumIndex, t);
+                        changeMade = true;
+                    }
                 }
+
+                correct = !changeMade;
             }
             #endregion
 
@@ -255,13 +269,11 @@ namespace Test
                     {
                         var randomIndex = random.Next(selectedJobs.Count);
                         var randomJob = selectedJobs[randomIndex];
-                        if(randomJob != job)
-                        {
-                            dependencyJob = randomJob;
-                            found = true;
-                        }
+                        if (randomJob == job) continue;
+                        dependencyJob = randomJob;
+                        found = true;
                     }
-                    finalString += string.Format("{0}>{1}", job, dependencyJob);
+                    finalString += $"{job}>{dependencyJob}";
                 }
                 else
                 {
